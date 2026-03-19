67c93bdc9c9ab500b3b113c897b41f6242e6f6be
tahamuhammad391 <tahamuhammad391@gmail.com>
2026-03-19 11:59:43 +0500

    Intro

---
+++ Intro_Variable_DataTy.py
@@ -0,0 +1,35 @@
+# Introduction to Python 
+
+# First used print statement -> Its is called single line Comments
+"""
+ This is 
+ multi line comments
+"""
+'''
+This is 
+multi line comments 
+'''
+print("Hello World!!!") # We can used "" as well as '' to write string in print statement.
+print('Hello World!!!') 
+
+# variables 
+# Rules to make a variable 
+# 1) Don't used number at the start of variable-> 1Number=12
+# 2) Don't used special Characters at the start of variable-> %Number=12 or $Name etc
+# 3) Only one charactor should be used -> _Name = "Taha"
+# 4) Don't used space between two variable name -> n1 name=12     -> X wrong 
+Name = "Taha"
+print(Name) # -> This will print Taha which is in the Name variable.
+print(type(Name)) # -> This will tells the type of Name variable, which is String. 
+sent = "My Age is 26 !!!"
+print(sent)
+# Data_Types 
+'''
+Numeric Data Type
+integer -> Whole number : 10, 25, 30,100
+float -> Decimal number : 21.45, 52.36,12.45
+complex -> real & Imaginary value -> 3+4j
+bolean Data Type 
+True/false -> It is used for logical Operations
+String Data Typev-> It is useed to store any Charactor, word or Sentence
+'''
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Python_Tutorial-
 **Introduction (Variables and Data_types)** 
+1) Comment 
+2) print Statment 
+3) Variables 
